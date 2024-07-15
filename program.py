f076561f7cd0d45dde6eef01e1e517d3e4165e66
sajadbehzadipoursb <sajadbehzadipour@gmail.com>
2024-07-15 09:25:55 +0330

    all files deleted

--- program.py
+++
@@ -1,3 +0,0 @@
-print ("Hello! this is first line")
-print ("Hello! this is 2nd line") 
-print ("Hello! this is 3rd line") 
--- step.py
+++
@@ -1,5 +0,0 @@
-print ("first step")
-print ("2nd step")
-print ("3rd step")
-
-
